3afafb1b3dcb5020ba1ea900d1daca2be2b5857e
Joir Neto <joirneto@febracis.com.br>
2024-01-05 14:45:15 -0300

    responsividade2

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -9,18 +9,18 @@ const Hero = () => {
         <div className='lg:pt-32 pt-16'>
             <h2 className="text-4xl pl-16">HI! I'M JOIR NETO</h2>
             <h2 className="font-bold text-4xl lg:text-5xl pl-16">SOFTWARE ENGINEER</h2>
-            <div className='relative border-orange border rounded px-16 pb-4 pt-10 mt-6'>
+            <div className='mx-8 px-8 relative border-orange border rounded md:px-16 pb-4 pt-10 mt-6'>
                 <h3 className='text-white bg-orange absolute py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase'>Contact me</h3>
-                <p>
-                    <FaLinkedin className='text-7xl inline-block mr-6' />
-                    <FaGithub className='text-7xl inline-block mr-6' />
-                    <IoLogoWhatsapp className='text-7xl inline-block mr-6' />
-                    <SiGmail className='text-7xl inline-block' />
+                <p className='text-center'>
+                    <FaLinkedin className='text-4xl md:text-7xl inline-block mr-6' />
+                    <FaGithub className='text-4xl md:text-7xl inline-block mr-6' />
+                    <IoLogoWhatsapp className='text-4xl md:text-7xl inline-block mr-6' />
+                    <SiGmail className='text-4xl md:text-7xl inline-block' />
                 </p>
             </div>
         </div>
-        <div>
-            <img style={{ borderBottomRightRadius: '10%'}} src='/images/profile.png' />
+        <div className='px-12'>
+            <img src='/images/profile.png' />
         </div>
     </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,20 @@ const Index = ({ user, repos }) => {
         <div className='container mx-auto bg-betwork'>
             <PageHead/>
             <Hero/>
-            <div className='bg-white roudend-lg shadow-lg py-12 px-16'>
+            <div className='bg-white roudend-lg shadow-lg py-12 px-8 mx-8 lg:px-16'>
                 <h3 className='text-4xl text-center text-orange'> What I do</h3>
                 <p className='text-2xl '>BACKEND DEVELOPER JS</p>
                 <p className='text-2xl'>AWS CLOULD</p>
             </div>
             <div>
                 <h3 className='text-orange text-4xl text-center font-bold uppercase mt-10 mb-5'>My Education</h3>
-                <div className='lg:grid lg:grid-cols-2 leading-none bg-white roudend-lg shadow-lg py-6'>
-                    <div className='border-dashed border-l px-12'>
+                <div className='mx-8 lg:grid lg:grid-cols-2 leading-none bg-white roudend-lg shadow-lg py-6'>
+                    <div className='border-dashed border-t lg:border-l lg:border-t-0 mx-6 px-6 lg:px-12 py-6'>
                         <h4 className='text-lg uppercase font-bold mb-2 text-orange'>Graduação</h4>
                         <p className='text-2xl uppercase'>Engenharia da Computação<br />
                             <span className='text-lg normal-case font-bold'>Universidade Federal do Ceará</span></p>
                     </div>
-                    <div className='border-dashed border-l px-12'>
+                    <div className='border-dashed border-t lg:border-l lg:border-t-0 mx-6 px-6 lg:px-12 py-6'>
                         <h4 className='text-lg uppercase font-bold mb-2 text-orange'>Graduação</h4>
                         <p className='text-2xl uppercase'>Engenharia da Computação<br />
                             <span className='text-lg normal-case font-bold'>Universidade Federal do Ceará</span></p>
@@ -31,12 +31,12 @@ const Index = ({ user, repos }) => {
                 </div>
             </div>
             <div>
-                <h3 className='text-orange text-4xl text-center font-bold uppercase mt-10'>Tech Contributions</h3>
+                <h3 className='text-orange lg:text-4xl text-3xl text-center font-bold uppercase mt-10'>Tech Contributions</h3>
                 <p className='text-center'>Github stats - <RiGitRepositoryLine className='inline-block' /> {user.public_repos} / <HiUsers className='inline-block' /> {user.followers}</p>
-                <div className='lg:grid lg:grid-cols-3 lg:gap-2 my-6'>
+                <div className='lg:grid lg:grid-cols-3 lg:gap-2 lg:my-6'>
                     {repos.map(repo => {
                         return (
-                            <div key={repo.id} className='rounded bg-white p-4 hover:shadow-md'>
+                            <div key={repo.id} className='rounded bg-white p-4 hover:shadow-md my-3 lg:my-0'>
                                 <h3 className='font-bold hover:underline'><a href={'https://github.com/' + repo.full_name}>{repo.full_name.replace('joirneto/', '').toUpperCase()}</a></h3>
                                 <h4>{repo.language}</h4>
                             </div>
