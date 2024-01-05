3600c6fd150d0df8d3b6bbcb7af4c955bc1fb417
Joir Neto <joirneto@febracis.com.br>
2024-01-05 14:11:25 -0300

    Responsividade1

---
+++ components/Hero/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaLinkedin, FaGithub, } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { SiGmail } from "react-icons/si";
+
+const Hero = () => {
+    return (
+        <div className='lg:grid lg:grid-cols-2 lg:pt-16 leading-none'>
+        <div className='lg:pt-32 pt-16'>
+            <h2 className="text-4xl pl-16">HI! I'M JOIR NETO</h2>
+            <h2 className="font-bold text-4xl lg:text-5xl pl-16">SOFTWARE ENGINEER</h2>
+            <div className='relative border-orange border rounded px-16 pb-4 pt-10 mt-6'>
+                <h3 className='text-white bg-orange absolute py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase'>Contact me</h3>
+                <p>
+                    <FaLinkedin className='text-7xl inline-block mr-6' />
+                    <FaGithub className='text-7xl inline-block mr-6' />
+                    <IoLogoWhatsapp className='text-7xl inline-block mr-6' />
+                    <SiGmail className='text-7xl inline-block' />
+                </p>
+            </div>
+        </div>
+        <div>
+            <img style={{ borderBottomRightRadius: '10%'}} src='/images/profile.png' />
+        </div>
+    </div>
+    )
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,15 @@
 import React from 'react';
-import Head from 'next/head';
 import getUser from '../utils/getUser';
-import { FaLinkedin, FaGithub, } from "react-icons/fa";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { SiGmail } from "react-icons/si";
 import { RiGitRepositoryLine } from "react-icons/ri";
 import { HiUsers } from "react-icons/hi2";
+import PageHead from '../components/PageHead';
+import Hero from '../components/Hero';
 
 const Index = ({ user, repos }) => {
     return (
-        <div className='container mx-auto'>
-            <Head>
-                <title>
-                    Joir Neto - Software Engineer
-                </title>
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-                <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/manifest.json"/>
-                <meta name="msapplication-TileColor" content="#ffffff"/>
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-                <meta name="theme-color" content="#ffffff"/>
-            </Head>
-            <div className='grid grid-cols-2 pt-16 leading-none'>
-                <div className='pt-32'>
-                    <h2 className="text-4xl pl-16">HI! I'M JOIR NETO</h2>
-                    <h2 className="font-bold text-5xl pl-16">SOFTWARE ENGINEER</h2>
-                    <div className='relative border-orange border rounded px-16 pb-4 pt-10 mt-6'>
-                        <h3 className='text-white bg-orange absolute py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase'>Contact me</h3>
-                        <p>
-                            <FaLinkedin className='text-7xl inline-block mr-6' />
-                            <FaGithub className='text-7xl inline-block mr-6' />
-                            <IoLogoWhatsapp className='text-7xl inline-block mr-6' />
-                            <SiGmail className='text-7xl inline-block' />
-                        </p>
-                    </div>
-                </div>
-                <div >
-                    <img className='pr-16' style={{ borderBottomRightRadius: '10%' }} src='/images/profile1.png' />
-                </div>
-            </div>
+        <div className='container mx-auto bg-betwork'>
+            <PageHead/>
+            <Hero/>
             <div className='bg-white roudend-lg shadow-lg py-12 px-16'>
                 <h3 className='text-4xl text-center text-orange'> What I do</h3>
                 <p className='text-2xl '>BACKEND DEVELOPER JS</p>
@@ -57,7 +17,7 @@ const Index = ({ user, repos }) => {
             </div>
             <div>
                 <h3 className='text-orange text-4xl text-center font-bold uppercase mt-10 mb-5'>My Education</h3>
-                <div className='grid grid-cols-2 leading-none bg-white roudend-lg shadow-lg py-6'>
+                <div className='lg:grid lg:grid-cols-2 leading-none bg-white roudend-lg shadow-lg py-6'>
                     <div className='border-dashed border-l px-12'>
                         <h4 className='text-lg uppercase font-bold mb-2 text-orange'>Graduação</h4>
                         <p className='text-2xl uppercase'>Engenharia da Computação<br />
@@ -73,7 +33,7 @@ const Index = ({ user, repos }) => {
             <div>
                 <h3 className='text-orange text-4xl text-center font-bold uppercase mt-10'>Tech Contributions</h3>
                 <p className='text-center'>Github stats - <RiGitRepositoryLine className='inline-block' /> {user.public_repos} / <HiUsers className='inline-block' /> {user.followers}</p>
-                <div className='grid grid-cols-3 gap-2 my-6'>
+                <div className='lg:grid lg:grid-cols-3 lg:gap-2 my-6'>
                     {repos.map(repo => {
                         return (
                             <div key={repo.id} className='rounded bg-white p-4 hover:shadow-md'>
