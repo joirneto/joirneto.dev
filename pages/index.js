920a0c59136bf831492edd6dd5a1a4ffe68b7c6e
Joir Neto <joirneto@febracis.com.br>
2024-01-05 10:25:57 -0300

    Incluindo repos

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import getUser from '../utils/getUser';
 import { FaLinkedin, FaGithub, } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { SiGmail } from "react-icons/si";
+import { RiGitRepositoryLine } from "react-icons/ri";
+import { HiUsers } from "react-icons/hi2";
 
-const Index = ({ user }) => {
+const Index = ({ user, repos }) => {
     return (
         <div className='container mx-auto'>
             <div className='grid grid-cols-2 pt-16 leading-none'>
@@ -14,15 +16,15 @@ const Index = ({ user }) => {
                     <div className='relative border-orange border rounded px-16 pb-4 pt-10 mt-6'>
                         <h3 className='text-white bg-orange absolute py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase'>Contact me</h3>
                         <p>
-                            <FaLinkedin  className='text-7xl inline-block mr-6'/>
-                            <FaGithub className='text-7xl inline-block mr-6'/>
-                            <IoLogoWhatsapp className='text-7xl inline-block mr-6'/>
-                            <SiGmail className='text-7xl inline-block'/>
+                            <FaLinkedin className='text-7xl inline-block mr-6' />
+                            <FaGithub className='text-7xl inline-block mr-6' />
+                            <IoLogoWhatsapp className='text-7xl inline-block mr-6' />
+                            <SiGmail className='text-7xl inline-block' />
                         </p>
                     </div>
                 </div>
                 <div >
-                    <img className='pr-16' style={{ borderBottomRightRadius: '10%' }} src='/images/profile.png' />
+                    <img className='pr-16' style={{ borderBottomRightRadius: '10%' }} src='/images/profile1.png' />
                 </div>
             </div>
             <div className='bg-white roudend-lg shadow-lg py-12 px-16'>
@@ -45,16 +47,30 @@ const Index = ({ user }) => {
                     </div>
                 </div>
             </div>
-            <p>Github stats - public repos: {user.public_repos} / followers: {user.followers}</p>
+            <div>
+            <h3 className='text-orange text-4xl text-center font-bold uppercase mt-10'>Tech Contributions</h3>
+            <p className='text-center'>Github stats - <RiGitRepositoryLine className='inline-block'/> {user.public_repos} / <HiUsers className='inline-block'/> {user.followers}</p>
+            <div className='grid grid-cols-3 gap-2 my-6'>
+            {repos.map(repo => {
+                return (
+                    <div key={repo.id} className='rounded bg-white p-4 hover:shadow-md'>
+                        <h3 className='font-bold hover:underline'><a href={'https://github.com/' + repo.full_name}>{repo.full_name.replace('joirneto/', '').toUpperCase()}</a></h3>
+                        <h4>{repo.language}</h4>
+                    </div>
+                )
+            })}
+            </div>
+            </div>
         </div>
     )
 }
 
 export async function getServerSideProps(context) {
-    const { user } = await getUser('joirneto');
+    const { user, repos } = await getUser('joirneto');
     return {
         props: {
             user,
+            repos,
         }
     }
 }
