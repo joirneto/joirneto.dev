bab985f63aa049443ef1328580c80175279e9422
joirneto <joirneto@gmail.com>
2024-01-15 16:56:37 -0300

    read-me

--- utils/getUser.js
+++ utils/getUser.js
@@ -3,7 +3,7 @@ const getUser = async (userName) => {
     const user = await resUser.json();
 
     const resRepos = await fetch(`https://api.github.com/users/${userName}/repos?sort=update`);
-    console.log(resRepos)
+
     const reposData = await resRepos.json();
     const repos = reposData.map(repo => {
         const {id, full_name, language} = repo;
