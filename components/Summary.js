7043dbb77c3fe980f0a9b5192307cad283b99803
Joir Neto <joirneto@febracis.com.br>
2024-02-13 10:33:08 -0300

    atualização

--- components/Summary.js
+++ components/Summary.js
@@ -1,13 +1,33 @@
 import React from "react";
 
 const Summary = () => {
-    return (
-        <div className='bg-white roudend-lg shadow-lg py-12 px-8 mx-8 lg:px-16'>
-            <h3 className='text-4xl text-center text-orange'> What I do</h3>
-            <p className='text-2xl '>BACKEND DEVELOPER JS</p>
-            <p className='text-2xl'>AWS CLOULD</p>
-        </div>
-    )
-}
+  return (
+    <div className="bg-white roudend-lg shadow-lg py-12 px-8 mx-8 lg:px-16">
+      <h3 className="text-4xl text-center text-orange"> What I do</h3>
+      <p className="text-3xl">
+        Software engineer | Back-End | Javascript | Node.js | Serverless | AWS
+        cloud | AdonisJs | NextJs
+      </p>
+      <p className="text-2xl">
+        JavaScript Backend Developer with up to 3 years of market experience.
+        Currently, my development stack has been focused on Javascript, working
+        with NodeJS, ExpressJS, Serverless, AWS Cloud (Lambda, DynamoDB,
+        Streams, SQS, SNS, Step Functions). I have previous experience with
+        NextJS, Typescript, Jest, MongoDB, HTML, CSS, TailwindCSS, SWR, Vercel,
+        among others. I also have experience in people management, leading
+        multidisciplinary teams, conducting follow-ups, and performing 360º
+        evaluations. I hold a degree in Computer Engineering from UFC, a
+        postgraduate degree in Information Security and Project Management, and
+        I am currently a backend developer at Febracis and a full-stack
+        developer at the Superintendence of Administration at BB. Passionate
+        about everything related to technology, I bring focus, cooperation, and
+        discipline to the development of work and the achievement of objectives.
+        I aspire to become a reference in the field, evolving technologies,
+        sharing knowledge with communities, and adding value to
+        clients/partners.
+      </p>
+    </div>
+  );
+};
 
 export default Summary;
